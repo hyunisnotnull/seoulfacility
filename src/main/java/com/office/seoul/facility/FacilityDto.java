aa8773594cbe6a07fdc6a0a56230a948fd74b86d
hyun <bongtacker@naver.com>
2024-09-09 17:49:35 +0900

    fix_api

--- src/main/java/com/office/seoul/facility/FacilityDto.java
+++ src/main/java/com/office/seoul/facility/FacilityDto.java
@@ -10,13 +10,20 @@ import lombok.NoArgsConstructor;
 public class FacilityDto {
 	
 	private int f_no;
+	private String gubun;		//서비스구분
 	private String svcid;		//서비스id
+	private String maxclassnm;	//대분류명
 	private String minclassnm;	//소분류명
 	private String svcstatnm;	//서비스상태
 	private String svcnm;		//서비스명
 	private String payatnm;		//결제방법
 	private String placenm;		//장소명
 	private String usetgtinfo;	//서비스대상
+	private String svcurl;		//바로가기url
+	private String x;			//장소x좌표
+	private String y;			//장소y좌표
+	private String svcopnbgndt;	//서비스개시시작일시
+	private String svcopnenddt;	//서비스개시종료일시
 	private String rcptbgndt;	//접수시작일시
 	private String rcptenddt;	//접수종료일시
 	private String areanm;		//지역명
