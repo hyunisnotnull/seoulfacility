7f2ed4edde0b238a3e0e261535bd9e2ca1b12c33
wkdwlsdjs <wkdwlsdjs@gmail.com>
2024-09-10 16:54:35 +0900

    until_member_modify

--- src/main/java/com/office/seoul/config/SecurityConfig.java
+++ src/main/java/com/office/seoul/config/SecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -27,11 +30,40 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 	    http
-	        .authorizeHttpRequests(authorizeRequests ->
-	            authorizeRequests
-	                .anyRequest().permitAll() // 모든 요청을 허용
-	        )
-	        .csrf(csrf -> csrf.disable());
+        .authorizeHttpRequests(authorizeRequests ->
+            authorizeRequests
+                .anyRequest().permitAll() // 모든 요청을 허용
+        )
+        .csrf(csrf -> csrf.disable());
+	    
+	    http
+    	.formLogin(login -> login
+    			.loginPage("/member/member_login_form")
+    			.loginProcessingUrl("/member/member_login_confirm")
+				.usernameParameter("u_m_id")
+				.passwordParameter("u_m_pw")
+				.successHandler((request, response, authentication) -> {
+					log.info("[MEMBER LOGIN SUCCESS]");
+					
+					RequestCache requestCache = new HttpSessionRequestCache();
+					SavedRequest savedRequest = requestCache.getRequest(request, response);
+					String targetURI = "/";
+					if (savedRequest != null) {
+						targetURI = savedRequest.getRedirectUrl();
+						requestCache.removeRequest(request, response);
+						
+					}
+					
+					response.sendRedirect(targetURI);
+					
+				})
+				.failureHandler((request, response, exception) -> {
+					log.info("[MEMBER LOGIN FAIL]");
+					log.info("Exception: {}", exception);
+					
+					response.sendRedirect("/member/member_login_form");
+					
+				}));
 
 	    return http.build();
 	}
---
+++ src/main/java/com/office/seoul/facility/member/MemberDetailService.java
@@ -0,0 +1,40 @@
+package com.office.seoul.facility.member;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Service
+public class MemberDetailService implements UserDetailsService {
+	
+	final private IMemberDao iMemberDao;
+	
+	public MemberDetailService(IMemberDao iMemberDao) {
+		this.iMemberDao = iMemberDao;
+		
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		MemberDto selectedMemberDto = 
+				iMemberDao.selectMemberByMId(username);
+		
+		if (selectedMemberDto != null) {
+			return User.builder()
+					.username(selectedMemberDto.getU_m_id())
+					.password(selectedMemberDto.getU_m_pw())
+					.build();
+			
+		}
+		
+		return null;
+		
+	}
+
+}
